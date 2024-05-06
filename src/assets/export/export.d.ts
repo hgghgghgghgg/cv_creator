9a67689f75298fe501aebb3d5b6080646b4cb162
hgghgghgghgg <slexandr87@gmail.com>
2024-05-06 19:49:54 +0300

    docs(cv): add a structure

---
+++ src/assets/export/export.d.ts
@@ -0,0 +1,64 @@
+declare module "*.png" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.jpeg" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.gif" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.pdf" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.raw" {
+  const value: ArrayBuffer;
+  export = value;
+}
+
+declare module "*.webp" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.mp4" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.webm" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.ico" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.tiff" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.bmp" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.avi" {
+  const value: string;
+  export = value;
+}
